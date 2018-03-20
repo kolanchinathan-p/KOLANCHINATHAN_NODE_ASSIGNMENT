33234c5377c6e0a6d2a4fcfd84ca4aff45dec8b0
Kolanchinathan Pichaipillai <kolanchinathan.pichaipillai@photoninfotech.net>
2018-03-20 13:25:43 +0530

    Eshint - Code alignment issue fixed

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 // Create MySQL connection.
 var pool = mysql.createPool({
-    connectionLimit : 10, 
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "nodejs" //Database Name
-  });
+	connectionLimit : 10, 
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "nodejs" //Database Name
+});
 
 pool.getConnection((err, conn) => {
-    if (err) throw err;
+	if (err) throw err;
 });
 
 module.exports = pool;
--- routes/category.js
+++ routes/category.js
@@ -4,76 +4,77 @@ const router = express.Router();
 let categories = [];
 let products = [];
 
-var store = require('./store.js');
+var store = require("./store.js");
 
 //1. List Category (eg URL: http://localhost:3000/api/category)
 router.get("/", (req, res, next) => {
-    store.getAllCategory( (err, rows) => {
-        if (err)
-        return next(err);
-        categories = rows;
-        if(categories.length > 0) {
-            res.status(200);
-            res.json(categories);
-        }
-        else{
-            let error = 'No Category List found';
-            res.status(204);
-            res.json({ error });
-        }
-    });
+    
+	store.getAllCategory((err, rows) => {
+		if (err)
+			return next(err);
+		categories = rows;
+		if(categories.length > 0) {
+			res.status(200);
+			res.json(categories);
+		}
+		else{
+			let error = "No Category List found";
+			res.status(204);
+			res.json({ error });
+		}
+	});
     
 });
 
 //Get All Products
 router.get("/getAllProducts", (req, res, next) => {
     
-    store.getAllProducts((err, rows) => {
-        if (err)
-            return next(err);
-            products = rows;
+	store.getAllProducts((err, rows) => {
+		if (err)
+			return next(err);
+		products = rows;
 
-            if(products.length > 0) {
-            res.status(200);
-            res.json(products);
-            }
-            else{
-            let error = 'No Products List found ';
-            res.status(204);
-            res.json({ error });
-            }
+		if(products.length > 0) {
+			res.status(200);
+			res.json(products);
+		}
+		else{
+			let error = "No Products List found ";
+			res.status(204);
+			res.json({ error });
+		}
             
-    });
+	});
    
 });
 
 //Get Products by CategoryID (eg URL: )
 router.get("/getProductCategoryId/:id", (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    if(isNaN(id)){
-        let error = 'Invalid category id';
-        res.status(400);
-        res.json({ error });
-    }
-    else {
-        store.getAllProductsByCategoryID(id, (err, rows) => {
-            if (err)
-              return next(err);
-              products = rows;
-    
-              if(products.length > 0) {
-                res.status(200);
-                res.json(products);
-              }
-              else{
-                res.status(204);
-                let error = 'No Products List found for the category id '+ id;
-                res.json({ error });
-              }
+	if(isNaN(id)){
+		let error = "Invalid category id";
+		res.status(400);
+		res.json({ error });
+	}
+	else {
+		store.getAllProductsByCategoryID(id, (err, rows) => {
+			if (err)
+				return next(err);
+			products = rows;
+
+			if(products.length > 0) {
+				res.status(200);
+				res.json(products);
+			}
+			else{
+				res.status(204);
+				let error = "No Products List found for the category id "+ id;
+				res.json({ error });
+			}
               
-        });
-    }
+		});
+	}
    
 });
 
